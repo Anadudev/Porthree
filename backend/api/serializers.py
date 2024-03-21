b03c4aa78ce8719ceb21f9f429f863f517855dd5
AnaduCG <godzionwin@gmail.com>
2024-03-21 11:42:40 +0100

    added docstrings to the serializers classes and view classes

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,5 @@
+""" declaring serializers that work very similar to Django's forms """
+
 from rest_framework import serializers
 from api.models import (
     UserDetails,
@@ -18,84 +20,112 @@ from api.models import (
 
 
 class UserDetailsSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the UserDetails instances into representations such as json"""
+
     class Meta:
         model = UserDetails
         fields = "__all__"
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Post instances into representations such as json"""
+
     class Meta:
         model = Post
         fields = "__all__"
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Project instances into representations such as json"""
+
     class Meta:
         model = Project
         fields = "__all__"
 
 
 class TagSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Tag instances into representations such as json"""
+
     class Meta:
         model = Tag
         fields = "__all__"
 
 
 class ToolSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Tool instances into representations such as json"""
+
     class Meta:
         model = Tool
         fields = "__all__"
 
 
 class SocialSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Social instances into representations such as json"""
+
     class Meta:
         model = Social
         fields = "__all__"
 
 
 class SkillSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Skill instances into representations such as json"""
+
     class Meta:
         model = Skill
         fields = "__all__"
 
 
 class EducationSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Education instances into representations such as json"""
+
     class Meta:
         model = Education
         fields = "__all__"
 
 
 class ExperienceSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Experience instances into representations such as json"""
+
     class Meta:
         model = Experience
         fields = "__all__"
 
 
 class RatingSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Rating instances into representations such as json"""
+
     class Meta:
         model = Rating
         fields = "__all__"
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Comment instances into representations such as json"""
+
     class Meta:
         model = Comment
         fields = "__all__"
 
 
 class ReplySerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Reply instances into representations such as json"""
+
     class Meta:
         model = Reply
         fields = "__all__"
 
 
 class ShareSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Share instances into representations such as json"""
+
     class Meta:
         model = Share
         fields = "__all__"
 
 
 class LikeSerializer(serializers.ModelSerializer):
+    """serializing and deserializing the Like instances into representations such as json"""
+
     class Meta:
         model = Like
         fields = "__all__"
--- backend/api/views.py
+++ backend/api/views.py
@@ -29,75 +29,103 @@ from .serializers import (
     CommentSerializer,
     ReplySerializer,
     ShareSerializer,
-    LikeSerializer
+    LikeSerializer,
 )
 
 
 class UserDetailsViewSet(viewsets.ModelViewSet):
+    """adds representations of the to to the API view"""
+
     queryset = UserDetails.objects.all().order_by("date_joined")
     serializer_class = UserDetailsSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    """adds representations of the Post to the API view"""
+
     queryset = Post.objects.all().order_by("created_at")
     serializer_class = PostSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    """adds representations of the Project to the API view"""
+
     queryset = Project.objects.all().order_by("created_at")
     serializer_class = ProjectSerializer
 
 
 class TagViewSet(viewsets.ModelViewSet):
+    """adds representations of the Tag to the API view"""
+
     queryset = Tag.objects.all().order_by("created_at")
     serializer_class = TagSerializer
 
 
 class ToolViewSet(viewsets.ModelViewSet):
+    """adds representations of the Tool to the API view"""
+
     queryset = Tool.objects.all().order_by("created_at")
     serializer_class = ToolSerializer
 
 
 class SocialViewSet(viewsets.ModelViewSet):
+    """adds representations of the Social to the API view"""
+
     queryset = Social.objects.all().order_by("created_at")
     serializer_class = SocialSerializer
 
 
 class SkillViewSet(viewsets.ModelViewSet):
+    """adds representations of the Skill to the API view"""
+
     queryset = Skill.objects.all().order_by("created_at")
     serializer_class = SkillSerializer
 
 
 class EducationViewSet(viewsets.ModelViewSet):
+    """adds representations of the Education to the API view"""
+
     queryset = Education.objects.all().order_by("created_at")
     serializer_class = EducationSerializer
 
 
 class ExperienceViewSet(viewsets.ModelViewSet):
+    """adds representations of the Experience to the API view"""
+
     queryset = Experience.objects.all().order_by("created_at")
     serializer_class = ExperienceSerializer
 
 
 class RatingViewSet(viewsets.ModelViewSet):
+    """adds representations of the Rating to the API view"""
+
     queryset = Rating.objects.all().order_by("created_at")
     serializer_class = RatingSerializer
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """adds representations of the Comment to the API view"""
+
     queryset = Comment.objects.all().order_by("created_at")
     serializer_class = CommentSerializer
 
 
 class ReplyViewSet(viewsets.ModelViewSet):
+    """adds representations of the Reply to the API view"""
+
     queryset = Reply.objects.all().order_by("created_at")
     serializer_class = ReplySerializer
 
 
 class ShareViewSet(viewsets.ModelViewSet):
+    """adds representations of the Share to the API view"""
+
     queryset = Share.objects.all().order_by("created_at")
     serializer_class = ShareSerializer
 
 
 class LikeViewSet(viewsets.ModelViewSet):
+    """adds representations of the Like to the API view"""
+
     queryset = Like.objects.all().order_by("created_at")
     serializer_class = LikeSerializer
