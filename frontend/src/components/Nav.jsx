54997c1f5201183055afaf72141b12236db631a1
AnaduCG <godzionwin@gmail.com>
2024-03-21 14:14:50 +0100

    frontend folder setup

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Nav = () => {
+  return (
+    <div>
+    <Logo/>
+    <div>Nav</div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ frontend/src/data/index.jsx
@@ -0,0 +1,17 @@
+const NavLink = [
+    {
+        id: 1,
+        title: "Home",
+        url: "#hero",
+    },
+    {
+        id: 2,
+        title: "About",
+        url: "#about",
+    },
+    {
+        id: 3,
+        title: "Portfolios",
+        url: "#portfolios",
+    },
+]
