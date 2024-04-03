9a1cce4f8f6461f3bda17558ee905944b17baa59
AnaduCG <godzionwin@gmail.com>
2024-04-03 06:03:34 +0100

    installing mui lab and implementing user posts and projects collection page

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import DrawerAppBar from '../components/Nav';
 import { UserNavLinks } from '../data/NavLinks';
 import Footer from '../components/Footer';
 import Breadcrumb from '../components/Breadcrumb';
 import { useLocation } from 'react-router-dom';
 import PageTitle from './PageTitle';
+import { useLoaderData } from 'react-router-dom';
+import GetUser from '../data/GetUser';
+import Box from '@mui/material/Box';
+import Masonry from '@mui/lab/Masonry';
+import PostCard from '../components/PortfolioSections/PostCard';
+import { getUserData } from '../data/GetUser';
 
 const Posts = () => {
-        PageTitle("Posts");
+    const id = useLoaderData();
+    const [user, setUser] = useState('')
+    const [posts, setPosts] = useState('')
+    useEffect(() => {
+        async function getter() {
+            setUser(await (GetUser(id)));
+            setPosts(await (getUserData(id.id, 'posts')));
+        }
+        getter();
+    }, [id])
+
+    PageTitle("Posts");
     return (
         <React.Fragment>
-            <DrawerAppBar pages={UserNavLinks}/>
-      <Breadcrumb path={useLocation()} />
+            <DrawerAppBar pages={UserNavLinks(user)} />
+            <Breadcrumb path={useLocation()} />
             <div>Posts</div>
+            <Box>
+                <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={2} className='flex justify-center border'>
+                    {posts && posts.map((post, index) => (
+                        // <Item sx={{ [heights[index]]: true  }}>
+                            <PostCard key={index} post={post} mode={'Blog Post'}/>
+                        // </Item>
+                    ))}
+                </Masonry>
+            </Box>
             <Footer />
         </React.Fragment>
     )
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import DrawerAppBar from '../components/Nav';
 import { UserNavLinks } from '../data/NavLinks';
 import Footer from '../components/Footer';
 import Breadcrumb from '../components/Breadcrumb';
 import { useLocation } from 'react-router-dom';
 import PageTitle from './PageTitle';
+import { useLoaderData } from 'react-router-dom';
+import GetUser from '../data/GetUser';
+import Box from '@mui/material/Box';
+import Masonry from '@mui/lab/Masonry';
+import PostCard from '../components/PortfolioSections/PostCard';
+import { getUserData } from '../data/GetUser';
+
 
 const Projects = () => {
+    const id = useLoaderData();
+    const [user, setUser] = useState('')
+    const [projects, setProjects] = useState('')
+    useEffect(() => {
+        async function getter() {
+            setUser(await (GetUser(id)));
+            setProjects(await (getUserData(id.id, 'projects')));
+        }
+        getter();
+    }, [id])
+
+    console.log(projects)
     PageTitle("Projects");
     return (
         <React.Fragment>
-            <DrawerAppBar pages={UserNavLinks} />
+            <DrawerAppBar pages={UserNavLinks(user)} />
             <Breadcrumb path={useLocation()} />
-            <div>Projects</div>
-            <Footer />
+            <Box>
+                <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={2}>
+                    {projects && projects.map((project, index) => (
+                        // <Item sx={{ [heights[index]]: true  }}>
+                            <PostCard key={index} post={project} mode={'Project'}/>
+                        // </Item>
+                    ))}
+                </Masonry>
+            </Box>            <Footer />
         </React.Fragment>
     )
 }
