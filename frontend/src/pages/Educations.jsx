4ab654826a6a432b2e5d5a8931411ae5e1678a56
AnaduCG <anadudev@gmail.com>
2024-04-20 04:22:04 +0100

    Custom color for educations view more page

--- frontend/src/pages/Educations.jsx
+++ frontend/src/pages/Educations.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { useLoaderData, useLocation } from "react-router-dom";
-import { GetRelation } from '../data/GetUser';
+import { useLoaderData } from "react-router-dom";
+import GetUser, { GetRelation } from '../data/GetUser';
 import AboutCard from '../components/PortfolioSections/AboutCard';
 import { Box } from "@mui/material";
 import ResponsiveAppBar from "../components/Nav";
@@ -8,17 +8,19 @@ import Footer from '../components/Footer';
 import { UserNavLinks } from "../data/NavLinks";
 import PageTitle from './PageTitle';
 import Breadcrumb from '../components/Breadcrumb';
-
+import { useLocation } from 'react-router-dom';
 const Educations = () => {
-  const user = useLoaderData();
-  PageTitle(user?.username + ' Educations');
+  const userId = useLoaderData();
+  PageTitle(userId?.username + ' Educations');
+  const [user, setUser] = useState("");
   const [educations, setEducations] = useState("");
   useEffect(() => {
     async function getData() {
-      setEducations((await GetRelation(`http://127.0.0.1:8000/api/users/${user.id}/educations`)).results);
+      setEducations((await GetRelation(`http://127.0.0.1:8000/api/users/${userId.id}/educations`)).results);
+      setUser((await GetUser(userId)));
     }
     getData();
-  }, [user])
+  }, [userId])
 
   // console.log(user);
   return (
@@ -27,9 +29,9 @@ const Educations = () => {
       <ResponsiveAppBar pages={UserNavLinks(user)} />
       <Box padding={{ xs: "10px", sm: "50px" }}>
         <Breadcrumb path={useLocation()} />
-      <Box className='flex justify-center'>
-        {educations?.length > 0 && <AboutCard education={educations} title={"Education"} custom={'max-w-[60rem]'} />}
-      </Box>
+        <Box className='flex justify-center'>
+          {educations?.length > 0 && <AboutCard education={educations} title={"Education"} custom={'max-w-[60rem]'} customize={user} />}
+        </Box>
       </Box>
       <Footer />
 
