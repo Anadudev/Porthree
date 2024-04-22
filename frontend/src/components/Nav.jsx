56fcbae7808d2a8049f8eca29b15ae84d19f7a01
AnaduCG <anadudev@gmail.com>
2024-04-22 22:26:53 +0100

    mui imports cleanup

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
+  ButtonGroup
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import { Link } from "react-router-dom";
-import ButtonGroup from '@mui/material/ButtonGroup';
 import { userTools } from '../data/NavLinks';
 
 const settings = userTools();
@@ -83,7 +85,7 @@ function ResponsiveAppBar({ pages, custom }) {
   // console.log(user);
 
   return (
-    <AppBar sx={{ bgcolor: custom?.primary_color ||'' }} position="sticky">
+    <AppBar sx={{ bgcolor: custom?.primary_color || '' }} position="sticky">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           {/* app logo */}
