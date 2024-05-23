4f1c945480aa7727962226e72323f4ba40787748
Anadudev <anadudev@gmail.com>
2024-05-23 07:03:00 +0100

    Filter component documentation

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,6 +3,11 @@ import {
   Paper, BottomNavigation, Typography,
 } from '@mui/material';
 
+/**
+ * Renders a footer component with the current year in the copyright text.
+ *
+ * @return {JSX.Element} The footer component.
+ */
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
