b5790854df577cbe172eba329aefd0bceaaf0e7d
Anadudev <anadudev@gmail.com>
2024-05-27 19:04:51 +0100

    Setting up root api route config

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -1,3 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: `http://localhost:8000`,
+    timeout: 1000,
+    // headers: { 'X-Custom-Header': 'foobar' }
+});
+
 // Define the API host with a fallback value
 const apiHost = "http://localhost:8000"; // to rectify : process.env.REACT_APP_API_HOST || "http://localhost:8000";
 
